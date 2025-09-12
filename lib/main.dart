995706127f098fb83e35bfe49216dec0ff0c65e0
6631501092 <6631501092@lamduan.mfu.ac.th>
2025-09-12 11:42:26 +0700

    Add random num

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application/week5/design2.dart';
+import 'package:flutter_application/week5/random_demo.dart';
 
 void main() {
-  runApp(MaterialApp(home: Design2()));
+  runApp(MaterialApp(home: RandomDemo()));
 }
